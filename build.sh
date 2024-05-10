63b4e509acbd9c956e36d4adee16c2f9078705a8
LukoOG <emmanueladesipe01@gmail.com>
2024-05-10 12:53:27 +0100

    render

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
-set -oerrexit
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
 
+# poetry install
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
-pythom manage.py migrate
+python manage.py migrate
